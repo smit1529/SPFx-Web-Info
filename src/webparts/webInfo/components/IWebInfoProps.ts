4debc32783047752007202e6845d6eeb2963956f
Piyush.Patel <Piyush.Patel@westrock.com>
2019-08-05 09:44:21 -0400

    initial commit

---
+++ src/webparts/webInfo/components/IWebInfoProps.ts
@@ -0,0 +1,3 @@
+export interface IWebInfoProps {
+  description: string;
+}
---
+++ src/webparts/webInfo/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IWebInfoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'WebInfoWebPartStrings' {
+  const strings: IWebInfoWebPartStrings;
+  export = strings;
+}
